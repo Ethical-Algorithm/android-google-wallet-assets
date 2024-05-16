227253945917de2507999dd189000e5b5483dae2
Joao Luis <bernard.luis@necam.com>
2024-05-16 12:18:02 +0100

    refactor: Updated AddToGoogleWallet Button, to be more adjusted;

--- googlewallet/src/main/java/net/ealgorithm/googlewallet/AddToGoogleWalletButton.kt
+++ googlewallet/src/main/java/net/ealgorithm/googlewallet/AddToGoogleWalletButton.kt
@@ -2,6 +2,7 @@ package net.ealgorithm.googlewallet
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
 import android.widget.RelativeLayout
 import net.ealgorithm.googlewallet.databinding.ViewAddToGooglewalletButtonBinding
 
@@ -15,15 +16,32 @@ import net.ealgorithm.googlewallet.databinding.ViewAddToGooglewalletButtonBindin
  */
 class AddToGoogleWalletButton @JvmOverloads constructor(context: Context,
                                                         attrs: AttributeSet? = null) :
-    RelativeLayout(context, attrs) {
+    RelativeLayout(context, attrs), View.OnClickListener {
 
     private val binding = ViewAddToGooglewalletButtonBinding.inflate(context.layoutInflater, this)
+    private var callback: (() -> Unit)? = null
 
     init {
-        isClickable = true
-        isFocusable = true
-        setBackgroundResource(R.drawable.googlewallet_button_background)
-        contentDescription =
-            R.string.add_to_googlewallet_button_content_description.idToString(context)
+        binding.apply {
+            root.apply {
+                isClickable = true
+                isFocusable = true
+                isFocusableInTouchMode = true
+                setBackgroundResource(R.drawable.googlewallet_button_background)
+                contentDescription =
+                    R.string.add_to_googlewallet_button_content_description.idToString(context)
+                setOnClickListener(this@AddToGoogleWalletButton)
+            }
+            googlewalletOverlayImageView.setOnClickListener(this@AddToGoogleWalletButton)
+            googlewalletLabelImageView.setOnClickListener(this@AddToGoogleWalletButton)
+        }
+    }
+
+    fun setOnButtonClick(callback: () -> Unit) {
+        this.callback = callback
+    }
+
+    override fun onClick(v: View?) {
+        callback?.invoke()
     }
 }

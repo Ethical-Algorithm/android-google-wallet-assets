eff658010b9601d0b9239fbfaa3ce3072a99055c
Joao Luis <bernard.luis@necam.com>
2024-11-26 16:35:35 +0000

    refactor: Added margins to AddToGoogleWalletButton component;

--- googlewallet/src/main/java/net/ealgorithm/googlewallet/AddToGoogleWalletButton.kt
+++ googlewallet/src/main/java/net/ealgorithm/googlewallet/AddToGoogleWalletButton.kt
@@ -14,34 +14,95 @@ import net.ealgorithm.googlewallet.databinding.ViewAddToGooglewalletButtonBindin
  * @param context
  * @param attrs
  */
-class AddToGoogleWalletButton @JvmOverloads constructor(context: Context,
-                                                        attrs: AttributeSet? = null) :
-    RelativeLayout(context, attrs), View.OnClickListener {
+class AddToGoogleWalletButton
+    @JvmOverloads
+    constructor(
+        context: Context,
+        attrs: AttributeSet? = null,
+    ) : RelativeLayout(context, attrs),
+        View.OnClickListener {
+        private val binding = ViewAddToGooglewalletButtonBinding.inflate(context.layoutInflater, this)
+        private var callback: (() -> Unit)? = null
 
-    private val binding = ViewAddToGooglewalletButtonBinding.inflate(context.layoutInflater, this)
-    private var callback: (() -> Unit)? = null
+        init {
+            val attributes =
+                context.obtainStyledAttributes(attrs, R.styleable.AddToGoogleWalletButton, 0, 0)
 
-    init {
-        binding.apply {
-            root.apply {
-                isClickable = true
-                isFocusable = true
-                isFocusableInTouchMode = true
-                setBackgroundResource(R.drawable.googlewallet_button_background)
-                contentDescription =
-                    R.string.add_to_googlewallet_button_content_description.idToString(context)
-                setOnClickListener(this@AddToGoogleWalletButton)
+            binding.apply {
+                root.apply {
+                    isClickable = true
+                    isFocusable = true
+                    isFocusableInTouchMode = true
+                    setBackgroundResource(R.drawable.googlewallet_button_background)
+                    contentDescription =
+                        R.string.add_to_googlewallet_button_content_description.idToString(context)
+                    setOnClickListener(this@AddToGoogleWalletButton)
+                }
+                googlewalletOverlayImageView.setOnClickListener(this@AddToGoogleWalletButton)
+
+                val horizontal = R.dimen.googlewallet_button_horizontal_margins.idToDimen(context)
+                val vertical = R.dimen.googlewallet_button_vertical_margins.idToDimen(context)
+                val marginsHorizontal =
+                    attributes.getDimensionPixelSize(
+                        R.styleable.AddToGoogleWalletButton_marginsHorizontal,
+                        -1,
+                    )
+                val marginsVertical =
+                    attributes.getDimensionPixelSize(
+                        R.styleable.AddToGoogleWalletButton_marginsVertical,
+                        -1,
+                    )
+                val marginStart =
+                    attributes.getDimensionPixelSize(
+                        R.styleable.AddToGoogleWalletButton_marginStart,
+                        if (marginsHorizontal == -1) horizontal else marginsHorizontal,
+                    )
+                val marginTop =
+                    attributes.getDimensionPixelSize(
+                        R.styleable.AddToGoogleWalletButton_marginTop,
+                        if (marginsVertical == -1) vertical else marginsVertical,
+                    )
+                val marginRight =
+                    attributes.getDimensionPixelSize(
+                        R.styleable.AddToGoogleWalletButton_marginEnd,
+                        if (marginsHorizontal == -1) horizontal else marginsHorizontal,
+                    )
+                val marginBottom =
+                    attributes.getDimensionPixelSize(
+                        R.styleable.AddToGoogleWalletButton_marginBottom,
+                        if (marginsVertical == -1) vertical else marginsVertical,
+                    )
+
+                googlewalletLabelImageView.apply {
+                    val layoutParams = layoutParams as RelativeLayout.LayoutParams
+                    layoutParams.setMargins(
+                        marginStart,
+                        marginTop,
+                        marginRight,
+                        marginBottom,
+                    )
+                }
+                googlewalletLabelImageView.setOnClickListener(this@AddToGoogleWalletButton)
             }
-            googlewalletOverlayImageView.setOnClickListener(this@AddToGoogleWalletButton)
-            googlewalletLabelImageView.setOnClickListener(this@AddToGoogleWalletButton)
+            attributes.recycle()
+        }
+
+        /**
+         * Set on button click
+         *
+         * @param callback
+         * @receiver
+         */
+        fun setOnButtonClick(callback: () -> Unit) {
+            this.callback = callback
+        }
+
+        /**
+         * On click
+         *
+         * @param v
+         */
+        override fun onClick(v: View?) {
+            callback?.invoke()
         }
     }
-
-    fun setOnButtonClick(callback: () -> Unit) {
-        this.callback = callback
-    }
-
-    override fun onClick(v: View?) {
-        callback?.invoke()
-    }
-}
--- googlewallet/src/main/java/net/ealgorithm/googlewallet/InternalExtensions.kt
+++ googlewallet/src/main/java/net/ealgorithm/googlewallet/InternalExtensions.kt
@@ -3,7 +3,25 @@ package net.ealgorithm.googlewallet
 import android.content.Context
 import android.view.LayoutInflater
 
+/**
+ * Layout inflater
+ */
 internal val Context.layoutInflater: LayoutInflater
     get() = LayoutInflater.from(this)
 
+/**
+ * Id to string
+ *
+ * @param context
+ * @return
+ */
 internal fun Int.idToString(context: Context?): String = context?.getString(this) ?: ""
+
+/**
+ * Convert Resource to Dimen Int DEFAULT VALUE: 1px
+ *
+ * @param context
+ * @sample R.dimen.appMargin.idToDimen(context)
+ * @return Int - Size
+ */
+internal fun Int.idToDimen(context: Context?): Int = context?.resources?.getDimensionPixelSize(this) ?: 1
